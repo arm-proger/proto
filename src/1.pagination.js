b1e230c90e22ebd0af5b443b7ea0ba4495fa74b5
Hovhannes Kocharyan <hovkocharyan@yandex.com>
2021-05-19 14:37:33 +0400

    Create(1. Pagination.js): Created file and resolved task

--- src/1.pagination.js
+++ src/1.pagination.js
@@ -34,20 +34,33 @@ const paginationMethods = {
     paginationMethods.currentPage++
     return Pagination
   },
+  // Prev page
+  prevPage: () => {
+    paginationMethods.currentPage++
+    return Pagination
+  },
+  // First page
+  firstPage: () => {
+    return paginationMethods.book[0]
+  },
+  // Last page
+  lastPage: () => {
+    return paginationMethods.book[paginationMethods.book.length - 1]
+  },
   // Go to page
   goToPage: (pageNumber) => {
     paginationMethods.currentPage = pageNumber
-  }
+  },
 }
 
 // Pagination object
 const Pagination = Object.create(paginationMethods)
 
 Pagination.init(alphabetArray, 4)
-console.log(Pagination.getPageItems()); // ["a", "b", "c", "d"]
-Pagination.nextPage(); // add the current page by one
-console.log(Pagination.getPageItems()); // ["e", "f", "g", "h"]
-Pagination.nextPage().nextPage(); // the ability to call chainable
-console.log(Pagination.getPageItems()); // ["m", "n", "o", "p"]
-Pagination.goToPage(3); // current page must be set to 3
-console.log(Pagination.getPageItems()); // ["i", "j", "k", "l"]
+console.log(Pagination.getPageItems()) // ["a", "b", "c", "d"]
+Pagination.nextPage() // add the current page by one
+console.log(Pagination.getPageItems()) // ["e", "f", "g", "h"]
+Pagination.nextPage().nextPage() // the ability to call chainable
+console.log(Pagination.getPageItems()) // ["m", "n", "o", "p"]
+Pagination.goToPage(3) // current page must be set to 3
+console.log(Pagination.getPageItems()) // ["i", "j", "k", "l"]
