a565246a0e5e5e1c9a8cf968c0ef2846856c5202
Hovhannes Kocharyan <hovkocharyan@yandex.com>
2021-05-19 14:27:39 +0400

    Create(1. Pagination.js): Created file and resolved task

---
+++ src/1.pagination.js
@@ -0,0 +1,53 @@
+// Alphabet array
+const alphabetArray = 'abcdefghijklmnopqrstuvwxyz'.split('')
+
+// Proto object
+const paginationMethods = {
+  book: undefined,
+  currentPage: 1,
+  init: (array, contentCount) => {
+    let pages = []
+
+    const pagesContents = array.reduce((acc, el) => {
+      if (pages.length === 0 || pages.length % contentCount !== 0) {
+        pages.push(el)
+      }
+
+      if (
+        array.indexOf(el) === array.length - 1 ||
+        pages.length % contentCount === 0
+      ) {
+        acc.push(pages)
+        pages = []
+      }
+      return acc
+    }, [])
+
+    paginationMethods.book = pagesContents
+  },
+  // Get current page
+  getPageItems: () => {
+    return paginationMethods.book[paginationMethods.currentPage - 1]
+  },
+  // Next page
+  nextPage: () => {
+    paginationMethods.currentPage++
+    return Pagination
+  },
+  // Go to page
+  goToPage: (pageNumber) => {
+    paginationMethods.currentPage = pageNumber
+  }
+}
+
+// Pagination object
+const Pagination = Object.create(paginationMethods)
+
+Pagination.init(alphabetArray, 4)
+console.log(Pagination.getPageItems()); // ["a", "b", "c", "d"]
+Pagination.nextPage(); // add the current page by one
+console.log(Pagination.getPageItems()); // ["e", "f", "g", "h"]
+Pagination.nextPage().nextPage(); // the ability to call chainable
+console.log(Pagination.getPageItems()); // ["m", "n", "o", "p"]
+Pagination.goToPage(3); // current page must be set to 3
+console.log(Pagination.getPageItems()); // ["i", "j", "k", "l"]
